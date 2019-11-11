b8cd4e3e4dcc4bcb814ecc728e3eef02bd17cede
KOngTNP <tanapol.bu@ku.th>
2019-11-11 16:05:05 +0700

    add django

--- explorer/urls.py
+++ explorer/urls.py
@@ -1,4 +1,4 @@
-"""Explorer URL Configuration
+"""explorer URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),    
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Explorer.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'explorer.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
