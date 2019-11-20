d241614a362a18464b5d6e79efd2742722992208
KOngTNP <tanapol.bu@ku.th>
2019-11-20 17:46:20 +0700

    link login to home

--- login/urls.py
+++ login/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from .views import HomePageView
+from .views import user_login ,HomePageView
+
+
+app_name = "login"
+
 urlpatterns = [
-    path('', HomePageView.as_view(), name='homepage'),
+    path('home', HomePageView.as_view(), name='home'),
+    path('login',user_login,name='loginpage'),
+
+
 ]
