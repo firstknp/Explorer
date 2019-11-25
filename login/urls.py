8e94b29ef737c587d0d8920f125029fc55cafe2e
KOngTNP <tanapol.bu@ku.th>
2019-11-25 23:52:41 +0700

    add register urls

--- login/urls.py
+++ login/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
-from .views import user_login ,HomePageView
+from .views import user_login ,HomePageView ,register
 
 
 app_name = "login"
 
 urlpatterns = [
     path('home', HomePageView.as_view(), name='home'),
-    path('login',user_login,name='loginpage'),
+    path('',user_login,name='loginpage'),
     path('', include('social_django.urls', namespace='social')),
+    path('register',register,name='register'),
 
 
 ]
