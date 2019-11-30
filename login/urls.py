e4322716bea4494bbb529ed6b89d6caf2824481c
Firstt Kannipa <firstknp@Firstts-MacBook-Pro.local>
2019-11-30 20:07:11 +0700

    create pages link

--- login/urls.py
+++ login/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
-from .views import user_login ,HomePageView
+from .views import user_login, HomePageView, ProfileView, PollsView, user_logout
 
 
 app_name = "login"
 
 urlpatterns = [
     path('home', HomePageView.as_view(), name='home'),
-    path('',user_login,name='loginpage'),
+    path('', user_login,name='loginpage'),
     path('', include('social_django.urls', namespace='social')),
-
+    path('profile', ProfileView.as_view(), name='profile'),
+    path('polls', PollsView.as_view(), name='polls'),
+    path('logout', user_logout, name='logout'),
 
 ]
--- login/views.py
+++ login/views.py
@@ -10,6 +10,12 @@ from django.contrib.auth.decorators import login_required
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
+class ProfileView(TemplateView):
+    template_name = 'profile.html'
+
+class PollsView(TemplateView):
+    template_name = 'polls.html'
+
 def index(request):
     return render(request, 'login/index.html')
 
@@ -23,7 +29,8 @@ def special(request):
 def user_logout(request):
     logout(request)
     # return HttpResponseRedirect(reverse('index'))
-    return redirect(reverse('login:user_login'))
+    # return redirect('loginpage')
+    return render(request, "loginpage.html", {})
 
 
 def user_login(request):
