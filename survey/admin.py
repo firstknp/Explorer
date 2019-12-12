cdd34458d65e63bdb59f819b51e4b726c07faf77
KOngTNP <tanapol.bu@ku.th>
2019-12-12 22:31:02 +0700

    add survey to  adminSite

--- survey/admin.py
+++ survey/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Survey, Question, Choice
+
+admin.site.register(Survey)
+admin.site.register(Question)
+admin.site.register(Choice)
