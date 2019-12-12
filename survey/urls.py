9b7891fe7b5818d6a49bc1a6a98860813dc65301
KOngTNP <tanapol.bu@ku.th>
2019-12-13 01:10:15 +0700

    add surver_assignment url

--- survey/urls.py
+++ survey/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  path('survey-assginment/<int:assignment_id>/', views.SurveyAssignmentView.as_view(), name='survey_assignment'),
   path('surveys/create/', views.SurveyCreateView.as_view(), name='survey_create'),
     ]
