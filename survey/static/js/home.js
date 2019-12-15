11b9688c39ab9f98805676ebf9491febf5745240
Firstt Kannipa <firstknp@Firstts-MacBook-Pro.local>
2019-12-15 16:36:34 +0700

    edit .style.width into 300px

--- survey/static/js/home.js
+++ survey/static/js/home.js
@@ -1,5 +1,5 @@
 function openNav() {
-    document.getElementById("myNav").style.width = "30%";
+    document.getElementById("myNav").style.width = "300px";
 }           
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
