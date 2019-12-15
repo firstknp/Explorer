7e9b7f2feed6612d2c47c4585b919b1daa6de4cb
KOngTNP <tanapol.bu@ku.th>
2019-12-16 01:08:05 +0700

    remove logout url

--- login/urls.py
+++ login/urls.py
@@ -6,11 +6,8 @@ from .views import user_login,  ProfileView, PollsView ,register
 app_name = "login"
 
 urlpatterns = [
-
     path('',user_login,name='loginpage'),
-    # path('logout/',user_logout,name='logout'),
     path('', include('social_django.urls', namespace='social')),
     path('register/',register,name='register'),
-    # path('logout/', user_logout, name='logout'),
 
 ]
